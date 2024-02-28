105848f5c543adf21d9648bf39f5b3875fdf98ec
jangmoonseok <cacao621@naver.com>
2024-02-28 16:16:07 +0900

    공통으로 사용할 기업회원,일반회원 DAO 생성 및 테스트 코드 작성

--- JOBGO-COMMON/src/main/java/com/flab/jobgo/common/entity/EnterpriseUser.java
+++ JOBGO-COMMON/src/main/java/com/flab/jobgo/common/entity/EnterpriseUser.java
@@ -5,20 +5,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /** 기업회원 | TB_ENTERPRISE_USER */
 @Entity
 @Table(name = "TB_ENTERPRISE_USER")
-@Builder
-@Getter
+@Getter	
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //Entity는 기본생성자가 필수이다. 하지만 무의미한 Entity객체 생성을 막기위해 접근제어자는 protected로 열어놓자
 public class EnterpriseUser {
 	// 기업회원ID
     @Id
-    @Column(name = "USER_ID")
-    @GeneratedValue
-    private Long userId;
+    @Column(name = "USER_ID", length = 32)
+    private String userId;
 
     // 이메일
     @Column(name = "EMAIL" ,nullable = false ,length = 64)
@@ -27,5 +29,15 @@ public class EnterpriseUser {
     // 비밀번호
     @Column(name = "PW" ,nullable = false ,length = 32)
     private String pw;
+
+    @Builder //의도가 명확하지 않은 변경을 막기위해 Setter를 사용하지 않았다. 그래서 의미있는 객체를 생성하기 위해 Builder를 사용
+	public EnterpriseUser(String userId, String email, String pw) {
+		super();
+		this.userId = userId;
+		this.email = email;
+		this.pw = pw;
+	}
+    
+    
 }
 
---
+++ JOBGO-COMMON/src/test/java/com/flab/jobgo/common/dao/UserRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.flab.jobgo.common.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.flab.jobgo.common.entity.EnterpriseUser;
+import com.flab.jobgo.common.entity.GeneralUser;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+	private final GeneralUserRepository generalUserRepo;
+	private final EnterPriseUserRepository enterpriseUserRepo;
+	
+	@Autowired
+	public UserRepositoryTest(GeneralUserRepository generalUserRepo, EnterPriseUserRepository enterpriseUserRepo) {
+		super();
+		this.generalUserRepo = generalUserRepo;
+		this.enterpriseUserRepo = enterpriseUserRepo;
+	}
+
+	@Test
+	@Transactional // 테스트데이터는 롤백처리
+	void userRegistAndLoginTest() {
+		GeneralUser generalUser = GeneralUser.builder()
+			.userId("userA")
+			.pw("1111")
+			.address("대전 동구")
+			.birth("20001111")
+			.contact("01011111111")
+			.email("abcd@abcd.com")
+			.gender("남")
+			.userName("홍길동")
+			.build();
+		
+		// 일반회원 등록
+		generalUserRepo.save(generalUser);
+		
+		EnterpriseUser enterpriseUser = EnterpriseUser.builder()
+			.userId("userB")
+			.pw("1234")
+			.email("aaaa@aaaa.com")
+			.build();
+		
+		// 기업회원 등록
+		enterpriseUserRepo.save(enterpriseUser);
+		
+		// 일반회원 로그인 -> 실패처리
+		GeneralUser findGeneralUser = generalUserRepo.findByUserIdAndPw("userA", "1234");
+		// 기업회원 로그인 -> 성공처리
+		EnterpriseUser findEnterpriseUser = enterpriseUserRepo.findByUserIdAndPw("userB", "1234");
+		
+		
+		assertThat(findGeneralUser).isNull();
+		assertThat(findEnterpriseUser).isNotNull();
+	}
+	
+}
