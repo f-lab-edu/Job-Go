3fad11c1d79b89cac12aee04292096dc56908775
jangmoonseok <cacao621@naver.com>
2024-03-04 15:57:30 +0900

    UserService 생성 및 JOBGO-COMMON 의존성 추가

---
+++ JOBGO-USER-SERVICE/src/main/java/com/flab/jobgo/user/JobgoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flab.jobgo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobgoUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobgoUserServiceApplication.class, args);
+	}
+
+}
