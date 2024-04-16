70849ca245ca69421f89c3c6015debd04044804f
jangmoonseok <cacao621@naver.com>
2024-04-16 20:53:08 +0900

    ApiGateWay 생성 및 Dependency 추가

---
+++ JOBGO-API-GATEWAY/src/main/java/com/flab/jobgo/gateway/JobgoApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.flab.jobgo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobgoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobgoApiGatewayApplication.class, args);
+	}
+
+}
